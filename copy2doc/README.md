a2316b82d129c29f67d101f85234e883155f9a9d
branhuang <branhuang@tencent.com>
2021-08-25 16:44:20 +0800

    feat: 终于支持clickboard修改了

--- copy2doc/README.md
+++ copy2doc/README.md
@@ -1,1 +1,4 @@
-- [ ] 现在能拿到图片的url，需要pin上`http://tapd.oa.com/`，这里看怎么再上传到docs.qq.com然后replace
+- [x] 现在能拿到图片的url，需要pin上`http://tapd.oa.com/`，这里看怎么再上传到docs.qq.com然后replace
+- [x] 出于兼容等问题、碰到的一些问题记录如下、最终还是直接用clipboardData.getData、setData
+  1. [blob怎么处理成ClipboardItem](https://stackoverflow.com/questions/63561767/typeerror-failed-to-construct-clipboarditem-failed-to-convert-value-to-blob)
+  2. [阮一峰教程](https://www.ruanyifeng.com/blog/2021/01/clipboard-api.html)
--- copy2doc/src/app.js
+++ copy2doc/src/app.js
@@ -7,6 +7,10 @@ export default class extends React.Component {
     this.state = { show: true };
   }
 
+  $ = (selector) => {
+    document.querySelectorAll(selector)[0];
+  };
+
   componentDidMount() {
     // demo 程序将粘贴事件绑定到 document 上
     document.addEventListener(
@@ -39,9 +43,10 @@ export default class extends React.Component {
         for (var i = 0; i < cbd.items.length; i++) {
           var item = cbd.items[i];
 
-          console.log(item.getAsString(e=> {
-            console.log(e);
-          }), item.getAsFile());
+          item.getAsString((e) => {
+            console.log("string===>", e);
+          });
+          // 'file===>', item.getAsFile()
           if (item.kind == "file") {
             var blob = item.getAsFile();
             if (blob.size === 0) {
@@ -55,19 +60,55 @@ export default class extends React.Component {
       false
     );
 
-    document.addEventListener('copy', function(e){
-      const data = e.clipboardData || window.clipboardData;
+    // document.addEventListener("copy", function (e) {
+    //   const data = window.clipboardData || e.clipboardData;
 
-      if(!data) return;
+    //   for (let i = 0; i < data.items.length; i++) {
+    //     const item = data.items[i];
+    //     if (item.type === "text/html") {
+    //       item.getAsString((e) => {
+    //         console.log(e);
+    //       });
+    //     }
+    //   }
 
-      let text = window.getSelection();
+    //   if (!data) return;
 
-      console.log(text, text.toString());
+    //   let text = window.getSelection();
 
-      // e.clipboardData.setData('text/plain', 'Hello, world!');
-      // e.clipboardData.setData('text/html', '<b>Hello, world!</b>');
-      e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
-  });
+    //   console.log(text, text.toString());
+
+    //   // e.clipboardData.setData('text/plain', 'Hello, world!');
+    //   e.clipboardData.setData(
+    //     "text/html",
+    //     `<h4><strong>图：</strong></h4>
+    //     <p><img src="http://tapd.oa.com/tfl/pictures/202108/tapd_20425357_1628497502_68.png" height="453" style="width: 767px;" /></p>`
+    //   );
+    //   e.preventDefault(); // We want our data, not data from any selection, to be written to the clipboard
+    // });
+  }
+
+  handleCopy() {
+    $("#description-inline-edit").click();
+
+    setTimeout(() => {
+      document.getElementById("CommentDescriptionEditor_ifr").contentWindow.document.addEventListener("copy", async (e) => {
+        const data = window.clipboardData || e.clipboardData;
+        e.preventDefault();
+
+        let typeArr = data.types;
+
+        for (let i = 0; i < data.items.length; i++) {
+          const type = typeArr[i];
+          const data = e.clipboardData.getData(type);
+          e.clipboardData.setData(type, data.replace(
+            /([-a-zA-Z0-9@:%_+.~#?&//=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)?)/g,
+            ($1) => `http://tapd.oa.com${$1}`
+          ));
+          console.log(data);
+        }
+      });
+    }, 1000);
   }
 
   handleClose() {
@@ -76,6 +117,10 @@ export default class extends React.Component {
     });
   }
   render() {
-    return null;
+    return (
+      <button style={{ position: "absolute", right: 0, bottom: 0 }} onClick={this.handleCopy}>
+        复制
+      </button>
+    );
   }
 }
